6b3203b371f5717c37c040ccda77988c10c39fd5
catarinafaria16 <up202405128@up.pt>
2025-04-26 18:39:36 +0100

    Criação de dois objetos e utilizar o método toString para os apresentar no ecrã

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         Automovel a1 = new Automovel("11-11-AA", "Toyota",1400);
+        System.out.println(a1.toString());
+        Automovel a2 = new Automovel("22-22-BB","Fiat",0);
+        System.out.println(a2.toString());
+
     }
     }
