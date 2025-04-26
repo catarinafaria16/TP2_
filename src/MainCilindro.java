e9e82700a408e66914a512f2081936c5acd5dec4
catarinafaria16 <up202405128@up.pt>
2025-04-26 18:55:09 +0100

    Criação de objetos no MainCilindro

--- src/MainCilindro.java
+++ src/MainCilindro.java
@@ -1,5 +1,11 @@
 public class MainCilindro {
     public static void main(String[] args) {
+        Cilindro cil1 = new Cilindro();
+        cil1.setRaio(2);
+        cil1.setAltura(3);
 
+        Cilindro cil2 =new Cilindro();
+        cil2.setRaio(3);
+        cil2.setAltura(5);
     }
 }
