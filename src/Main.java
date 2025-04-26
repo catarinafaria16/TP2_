f905909f77738512968e3f591d6761d0f57899bb
catarinafaria16 <up202405128@up.pt>
2025-04-26 18:35:53 +0100

    Criação do construtor a1

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Automovel a1 = new Automovel("11-11-AA", "Toyota",1400);
+    }
+    }
