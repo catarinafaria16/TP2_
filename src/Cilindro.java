e54aec1d4e2ef929b2fcfc44d008f1ccc68c30bc
catarinafaria16 <up202405128@up.pt>
2025-04-26 18:53:38 +0100

    Criação de variáveis e de métodos de acesso

--- src/Cilindro.java
+++ src/Cilindro.java
@@ -1,2 +1,20 @@
 public class Cilindro {
+    private double raio;
+    private double altura;
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 }
