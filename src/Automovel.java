20c9749187be43c17b04e9ad627b2e73ba477f85
catarinafaria16 <up202405128@up.pt>
2025-04-26 18:30:27 +0100

    Criação dao construtor Automóvel

--- src/Automovel.java
+++ src/Automovel.java
@@ -2,4 +2,11 @@ public class Automovel {
     private String matricula;
     private String marca;
     private int cilindrada;
+
+    public Automovel(String matricula, String marca, int cilindrada) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.cilindrada = cilindrada;
+
+    }
 }
